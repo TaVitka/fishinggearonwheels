ae4df4c4630ecc275896df8454105ba0bc5383e9
TaVitka <tavitka17@gmail.com>
2024-04-28 00:11:31 +0300

    added blog section for homepage

---
+++ src/js/components/_slider.js
@@ -0,0 +1,23 @@
+import Swiper, { Navigation, Pagination, Scrollbar, Autoplay } from 'swiper';
+document.addEventListener('DOMContentLoaded', function() {
+
+  let options = {
+    slidesPerView: 'auto',
+    spaceBetween: 24,
+    centeredSlides: true,
+    speed: 700,
+    initialSlide: 1,
+    autoplay: {
+      delay: 50000,
+    },
+    modules: [Navigation, Scrollbar, Pagination, Autoplay],
+    draggable: true,
+    grabCursor: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  }
+
+  const swiperSpecials = new Swiper('.slider__posts', options);
+});
